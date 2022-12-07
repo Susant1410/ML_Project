9f37e12c4bda24ffd9aa6234f5e7a86898bb4664
Susant1410 <kumarsusant.bbl@gmail.com>
2022-12-07 23:47:22 +0530

    Update

--- app.py
+++ app.py
@@ -1,9 +1,17 @@
 from flask import Flask
-
+import sys
+from housing.logger import logging
+from housing.exception import HousingException
 app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
+    try:
+        raise Exception("Error message")
+    except Exception as e:
+            housing = HousingException(e,sys)
+            logging.info("Housing Error Message")
+            logging.info("Test the project")
     return "My First ML Projct"
 
 if __name__=="__main__":
