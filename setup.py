8a12210a9f4496e00b100a9347878e4b53b5e8f5
Susant1410 <kumarsusant.bbl@gmail.com>
2022-12-07 21:23:39 +0530

    Files updated

--- setup.py
+++ setup.py
@@ -1,26 +1,26 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 
 
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR="Susanta Panda"
 DESCRIPTION="This is the first ML project"
-PACKAGES=["housing"]
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
 def get_requirements_list()->List[str]:
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-      return requirement_file.readlines()
+      return requirement_file.readlines().remove("-e .")
 
 setup(
     name=PROJECT_NAME,
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 
 )
